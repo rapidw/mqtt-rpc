9d81c3b3e558ad8fa9e6ed66a3cbca08d2b5ccc1
pvtyuan <yyz139@gmail.com>
2023-08-14 17:16:38 +0800

    some code

--- server/src/main/java/io/rapidw/mqttrpc/server/controller/ClientController.java
+++ server/src/main/java/io/rapidw/mqttrpc/server/controller/ClientController.java
@@ -2,9 +2,11 @@ package io.rapidw.mqttrpc.server.controller;
 
 import io.rapidw.mqttrpc.server.model.Client;
 import io.rapidw.mqttrpc.server.service.ClientService;
+import io.rapidw.vo.response.BaseResponse;
 import io.rapidw.vo.response.DataResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +21,11 @@ public class ClientController {
     @GetMapping
     public DataResponse<Collection<Client>> query() {
         return DataResponse.of(clientService.query());
+    }
+
+    @PostMapping("/update")
+    public BaseResponse update(UpdateRequest request) {
+        clientService.update(request);
+        return BaseResponse.SUCCESS;
     }
 }
---
+++ server/src/main/java/io/rapidw/mqttrpc/server/controller/DriverController.java
@@ -0,0 +1,11 @@
+package io.rapidw.mqttrpc.server.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/driver")
+public class DriverController {
+
+    @
+}
